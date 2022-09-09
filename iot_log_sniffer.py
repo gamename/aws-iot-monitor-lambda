c8ce98b07785fa2e6bab1fb28c7ee7ae5a694a37
gamename <tennis.n.smith@gmail.com>
2022-09-09 17:18:55 -0500

    add lambda_handler function

--- iot_log_sniffer.py
+++ iot_log_sniffer.py
@@ -4,44 +4,46 @@ import time
 import json
 import os
 
-arn = os.environ.get('SNS_ARN')
 
-if not arn:
-    raise Exception('SNS_ARN not provided')
+def lambda_handler(event, context):
+    arn = os.environ.get('SNS_ARN')
 
-client = boto3.client('logs')
+    if not arn:
+        raise Exception('SNS_ARN not provided')
 
-query = "fields @timestamp, @message, @logStream | filter @message like /MSG00[34]/ | sort @timestamp desc | limit 20"
+    client = boto3.client('logs')
 
-log_group = 'AWSIotLogsV2'
+    query = "fields @timestamp, @message, @logStream | filter @message like /MSG00[34]/ | sort @timestamp desc"
 
-start_query_response = client.start_query(
-    logGroupName=log_group,
-    startTime=int((datetime.today() - timedelta(hours=12)).timestamp()),
-    endTime=int(datetime.now().timestamp()),
-    queryString=query,
-)
+    log_group = 'AWSIotLogsV2'
 
-query_id = start_query_response['queryId']
-
-response = None
-
-while response is None or response['status'] == 'Running':
-    print('Waiting for query to complete ...')
-    time.sleep(1)
-    response = client.get_query_results(
-        queryId=query_id
+    start_query_response = client.start_query(
+        logGroupName=log_group,
+        startTime=int((datetime.today() - timedelta(hours=12)).timestamp()),
+        endTime=int(datetime.now().timestamp()),
+        queryString=query,
     )
 
-if not response['results']:
-    print("No results found. Sending SMS")
-    message = {"MSG005": "Monitor offline"}
-    client = boto3.client('sns')
-    response = client.publish(
-        TargetArn=arn,
-        Message=json.dumps({'default': json.dumps(message)}),
-        MessageStructure='json'
-    )
-else:
-    for result in response['results']:
-        print(result)
+    query_id = start_query_response['queryId']
+
+    response = None
+
+    while response is None or response['status'] == 'Running':
+        print('Waiting for query to complete ...')
+        time.sleep(1)
+        response = client.get_query_results(
+            queryId=query_id
+        )
+
+    if not response['results']:
+        print("No results found. Sending SMS")
+        message = {"MSG005": "Monitor offline"}
+        client = boto3.client('sns')
+        response = client.publish(
+            TargetArn=arn,
+            Message=json.dumps({'default': json.dumps(message)}),
+            MessageStructure='json'
+        )
+    else:
+        for result in response['results']:
+            print(result)
